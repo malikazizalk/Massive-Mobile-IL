bb96658682cbaa1487631f0721c961d17eba26dc
cantguardput <puruardika@gmail.com>
2023-11-22 23:21:42 +0700

    test

--- app/src/main/java/com/example/massive_lesung/SplashScreenActivity.kt
+++ app/src/main/java/com/example/massive_lesung/SplashScreenActivity.kt
@@ -1,11 +1,18 @@
 package com.example.massive_lesung
-
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+
 
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+        Handler().postDelayed({
+            startActivity(Intent(this, RegisterActivity::class.java))
+            finish()
+        }, 1000)
+
     }
 }
--- app/src/main/java/com/example/massive_lesung/profile.kt
+++ app/src/main/java/com/example/massive_lesung/profile.kt
@@ -1,11 +1,11 @@
 package com.example.massive_lesung
 
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
 
-class LoginActivity : AppCompatActivity() {
+class profile : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_profile)
     }
 }
