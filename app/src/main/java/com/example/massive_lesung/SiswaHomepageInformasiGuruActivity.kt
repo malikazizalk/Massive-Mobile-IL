4355498882757de691ab5f247cd06f85fb20dc8e
malikaziz <azizak2119@gmail.com>
2023-12-05 04:19:03 +0800

    bagian siswa homepage fix

---
+++ app/src/main/java/com/example/massive_lesung/SiswaHomepageInformasiGuruActivity.kt
@@ -0,0 +1,11 @@
+package com.example.massive_lesung
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SiswaHomepageInformasiGuruActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_siswa_homepage_informasi_guru)
+    }
+}
