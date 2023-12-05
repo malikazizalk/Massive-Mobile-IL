49eb67e70a7ac103cb20cc41500cb4a59ed8837d
malikaziz <azizak2119@gmail.com>
2023-12-05 08:47:30 +0800

    bagian siswa kelas info

---
+++ app/src/main/java/com/example/massive_lesung/SiswaHomepageRekrutBayarActivity.kt
@@ -0,0 +1,11 @@
+package com.example.massive_lesung
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SiswaHomepageRekrutBayarActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_siswa_homepage_rekrut_bayar)
+    }
+}
