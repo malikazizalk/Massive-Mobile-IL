2cd4a3dfd53e6925a09bf3b912c167340ae83305
malikaziz <azizak2119@gmail.com>
2023-12-05 08:47:30 +0800

    added complete, profil, payement, and some values, drawable, font

---
+++ app/src/main/java/com/example/massive_lesung/SiswaProfileActivity.kt
@@ -0,0 +1,11 @@
+package com.example.massive_lesung
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SiswaProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_siswa_profile)
+    }
+}
